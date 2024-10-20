1419efe23a3cdb6503231e1f55113ac3b09d33a8
ranjeet <ranjeetkumargond000@gmail.com>
2024-10-21 00:08:17 +0530

    done

--- src/components/created-jobs.jsx
+++ src/components/created-jobs.jsx
@@ -1,9 +1,9 @@
 import { getMyJobs } from "@/api/apiJobs";
 import useFetch from "@/hooks/use-fetch";
 import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
 import { BarLoader } from "react-spinners";
 import JobCard from "./job-card";
-import { useEffect } from "react";
 
 const CreatedJobs = () => {
   const { user } = useUser();
@@ -18,31 +18,30 @@ const CreatedJobs = () => {
 
   useEffect(() => {
     fnCreatedJobs();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  if (loadingCreatedJobs) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
 
   return (
     <div>
-      {loadingCreatedJobs ? (
-        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
-      ) : (
-        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {createdJobs?.length ? (
-            createdJobs.map((job) => {
-              return (
-                <JobCard
-                  key={job.id}
-                  job={job}
-                  onJobAction={fnCreatedJobs}
-                  isMyJob
-                />
-              );
-            })
-          ) : (
-            <div>No Jobs Found 😢</div>
-          )}
-        </div>
-      )}
+      <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {createdJobs?.length ? (
+          createdJobs.map((job) => {
+            return (
+              <JobCard
+                key={job.id}
+                job={job}
+                onJobSaved={fnCreatedJobs}
+                isMyJob
+              />
+            );
+          })
+        ) : (
+          <div>No Jobs Found 😢</div>
+        )}
+      </div>
     </div>
   );
 };
